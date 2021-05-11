2a45f83ec45f484c330f9dd4ec6837486366af3d
Mandir <fcmh011@gmail.com>
2021-05-11 01:02:06 -0500

    initial commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700,800&display=swap" rel="stylesheet">
+    <script type="text/javascript" src="js/script.js"></script>
+</head>
+<body>
+<div class="principal">
+    <div class="contenedor_prin">
+     @yield("contenido")
+    </div>
+</div>
+</body>
+</html>
